4e108ee1dae0addb56652ec90b02d9ddb01aee56
osmar.betancourt@s4e.cl <osmar.betancourt@s4e.cl>
2022-09-18 23:00:20 -0400

    modificacion

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from main import obtiene_peso
 app = Flask(__name__)
 
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from bs4 import BeautifulSoup
 def obtiene_peso():
     response = requests.request("GET","https://openexchangerates.org/api/latest.json",params={"app_id":os.getenv("API_CURRENCY_KEY")})
     values = response.json()
-    return values['rates']['CLP']
+    try:
+        return values['rates']['CLP']
+    except:
+        return "Ocurrió un error"
     # URL = "https://www.google.com/search?q=dolar+a+peso+chileno"
     # r = requests.get(URL)
 
